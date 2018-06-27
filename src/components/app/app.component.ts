5bfcb215dd151c92d3ac81ab09904a5b29ffb893
SCP002 <feedusfeedme@gmail.com>
2018-06-27 14:54:45 +0300

    Add TODO's

--- src/components/app/app.component.ts
+++ src/components/app/app.component.ts
@@ -1,5 +1,8 @@
 import {Component} from '@angular/core';
 
+// TODO: See https://www.npmjs.com/package/ng-gapi
+// TODO: See https://developers.google.com/youtube/v3/getting-started
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
