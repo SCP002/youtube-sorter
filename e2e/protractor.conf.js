80fde358222c74c5d16dbd7a2f300ce553c6a0fc
SCP002 <feedusfeedme@gmail.com>
2018-10-08 07:12:43 +0300

    Format e2e files

--- e2e/protractor.conf.js
+++ e2e/protractor.conf.js
@@ -1,7 +1,7 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const {SpecReporter} = require('jasmine-spec-reporter');
+const { SpecReporter } = require('jasmine-spec-reporter');
 
 exports.config = {
     allScriptsTimeout: 11000,
@@ -24,6 +24,6 @@ exports.config = {
         require('ts-node').register({
             project: require('path').join(__dirname, './tsconfig.e2e.json')
         });
-        jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
+        jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
     }
 };
